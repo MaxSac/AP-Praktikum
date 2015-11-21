8262c639b6368a84cb8472e21b4157db1963d309
Maximilian Sackel <maximilian.sackel@gmx.de>
2015-11-21 17:57:24 +0100

    Abschluss des Kapitel Güteziffer

--- 05-17.November/plot.py
+++ 05-17.November/plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
+from uncertainties import ufloat
 
 #Einlesen der Daten
 t, T1, pb, T2, pa, W = np.loadtxt('data.txt', unpack=True)
@@ -33,12 +34,23 @@ plt.legend()
 plt.tight_layout()
 plt.savefig('build/Ausgleichsgrade.pdf')
 
+print("-----------------------------------------------------------")
+
 #Differentialquotient
 def q1(t):
     return 2*z1[0]*t + z1[1]
 def q2(t):
     return 2*z2[0]*t + z2[1]
-
 print('DiffQ T1', '60: T', p1(60),' dq 60', q1(60), ', 400:', p1(400),' dq 400', q1(400), ', 1000:', p1(1000), 'dq 1000', q1(1000), ', 1500:', p1(1500),'dq 1500 ', q1(1500))
 print('DiffQ T2', '60: T', p2(60),' dq 60', q2(60), ', 400:', p2(400),' dq 400', q2(400), ', 1000:', p2(1000), 'dq 1000', q2(1000), ', 1500:', p2(1500),'dq 1500 ', q2(1500))
 
+print("-----------------------------------------------------------")
+
+#Bestimmung der Güte
+mw = ufloat(4, 0.0016)
+cw = 4200
+qr = ufloat(750, 10)
+qg = mw*cw + qr
+wk = ufloat(np.mean(W), np.std(W))
+wk = wk*100
+print("Güte nach 60 sec : ", qg*q1(60)*(1/wk),", Güte nach 400 sec : ", qg*q1(400)*(1/wk),", Güte nach 1000 sec : ", qg*q1(1000)*(1/wk), "Güte nach 1500 sec : ", qg*q1(1500)*(1/wk))
