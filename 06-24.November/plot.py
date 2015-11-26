9a4497e7bd5c7f5378506805b6ad0a8b1fa32dc9
Philip Schäfers <phil.schaefers@gmail.com>
2015-11-26 21:39:14 +0100

    Sach mal was da Falsch ist

--- 06-24.November/plot.py
+++ 06-24.November/plot.py
@@ -1,21 +1,36 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import uncertainties.unumpy as unp
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
+from scipy import stats
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+#Einlesen der Daten und auf SI-Einheit bringen
+T, W, M, S, G = np.genfromtxt('data.txt', unpack=True)
+T = T + 273.15
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+#Stefan-Boltzmann-Konstante
+SB = 5.67 * 10**(-8)
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+#Offsetspannung "gemittel"
+offs = ((0.013 + 0.006)/2) * 10**(-3)
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+#T_0 bestimmen
+T_0 = 294.26
+print("T0:", 294.26)
+print("T:", T[0], T[1], T[2], T[3], T[4], T[5], T[6], T[7], T[8], T[9], T[10], T[11], T[12])
+
+
+#Thermospannung gegen Temperatur
+x = T**4 - T_0**4
+plt.plot(x, W)
+plt.xlabel(r'T**4 - T0**4 / K')
+plt.ylabel(r'U1 / mV')
+plt.legend()
+plt.tight_layout()
+plt.savefig('build/ThermospannungW.pdf')
+plt.close()
+
+#lineare Regression
+slope, intercept, r_value, p_value, std_err = stats.linregress(x, W)
+print('Steigung: ', slope, intercept, r_value, p_value, std_err)
