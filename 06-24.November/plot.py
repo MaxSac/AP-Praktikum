0987e1542583dc9ff06aac02f124582d6a587454
Philip Schäfers <phil.schaefers@gmail.com>
2015-11-27 15:57:05 +0100

    nur noch die Fehlerrechnung fehlt

--- 06-24.November/plot.py
+++ 06-24.November/plot.py
@@ -5,15 +5,19 @@ from scipy.optimize import curve_fit
 from uncertainties import ufloat
 from scipy import stats
 
-#Einlesen der Daten und auf SI-Einheit bringen
-T, W, M, S, G = np.genfromtxt('data.txt', unpack=True)
-T[0] = 95
-T = T + 273.15
-
 #Wichtige Werte
 SB = 5.67 * 10**(-8)
 offs = ((0.013 + 0.006)/2) * 10**(-3)
 T_0 = 294.26
+
+#Einlesen der Daten und auf SI-Einheit bringen
+T, W, M, S, G = np.genfromtxt('ThermoTemperatur.txt', unpack=True)
+T[0] = 95
+T = T + 273.15
+W = W * 10**(-3) - offs
+M = M * 10**(-3) - offs
+S = S * 10**(-3) - offs
+G = G * 10**(-3) - offs
 x = T**4 - T_0**4
 
 #Thermospannung gegen Temperatur(Weiß)
@@ -23,7 +27,7 @@ pW = np.poly1d(zW)
 plt.plot(x, W, 'g.', label='Messwerte')
 plt.plot(x, pW(x), 'b-', label='Ausgleichsgerade')
 plt.xlabel(r'$T^4 - T_0^4$ / K$^4$')
-plt.ylabel(r'U / $10^{-3}$ V')
+plt.ylabel(r'$U_1 - U_0$ / V')
 plt.legend(loc='upper left')
 plt.tight_layout()
 plt.savefig('build/ThermoW.pdf')
@@ -36,7 +40,7 @@ pM = np.poly1d(zM)
 plt.plot(x, M, 'g.', label='Messwerte')
 plt.plot(x, pM(x), 'b-', label='Ausgleichsgerade')
 plt.xlabel(r'$T^4 - T_0^4$ / K$^4$')
-plt.ylabel(r'U / $10^{-3}$ V')
+plt.ylabel(r'$U_2 - U_0$ / V')
 plt.legend(loc='upper left')
 plt.tight_layout()
 plt.savefig('build/ThermoM.pdf')
@@ -49,7 +53,7 @@ pS = np.poly1d(zS)
 plt.plot(x, S, 'g.', label='Messwerte')
 plt.plot(x, pS(x), 'b-', label='Ausgleichsgerade')
 plt.xlabel(r'$T^4 - T_0^4$ / K$^4$')
-plt.ylabel(r'U / $10^{-3}$ V')
+plt.ylabel(r'$U_3 - U_0$ / V')
 plt.legend(loc='upper left')
 plt.tight_layout()
 plt.savefig('build/ThermoS.pdf')
@@ -62,8 +66,25 @@ pG = np.poly1d(zG)
 plt.plot(x, G, 'g.', label='Messwerte')
 plt.plot(x, pG(x), 'b-', label='Ausgleichsgerade')
 plt.xlabel(r'$T^4 - T_0^4$ / K$^4$')
-plt.ylabel(r'U / $10^{-3}$ V')
+plt.ylabel(r'$U_4 - U_0$ / V')
 plt.legend(loc='upper left')
 plt.tight_layout()
 plt.savefig('build/ThermoG.pdf')
+plt.close()
+
+#Thermospannung gegen den Abstand(weiß)
+A, B = np.genfromtxt('ThermoAbstand.txt', unpack=True)
+A = A * 10**(-2)
+B = B * 10**(-3) - offs
+
+zB = np.polyfit(A, B, 1)
+print('Thermospannung / Abstand (weiß): ', zB)
+pB = np.poly1d(zB)
+plt.plot(A, B, 'g.', label='Messwerte')
+plt.plot(A, pB(A), 'b-', label='Ausgleichsgerade')
+plt.xlabel(r'A / m')
+plt.ylabel(r'U - $U_0$ / V')
+plt.legend(loc='upper right')
+plt.tight_layout()
+plt.savefig('build/ThermoAbstand.pdf')
 plt.close()
