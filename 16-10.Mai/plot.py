9890930257c12f9dd2a2f8c5e153579fe8695119
Philip Schäfers <phil.schaefers@gmail.com>
2016-05-14 14:24:50 +0200

    theorie zu 14

--- 16-10.Mai/plot.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.optimize import curve_fit
-from scipy.signal import find_peaks_cwt
-from uncertainties import ufloat
-import math
-
-B_250, B_300, B_350, B_400, B_450 = np.loadtxt('data/B-Feld.txt', unpack=True)
-E_230, E_300, E_350, E_180, E_260 = np.loadtxt('data/E-Feld.txt', unpack=True)
