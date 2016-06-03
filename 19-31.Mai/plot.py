1b2dd49a87a9ce2999685323a843ac651fac7f51
Philip Schäfers <phil.schaefers@gmail.com>
2016-06-03 08:53:31 +0200

    kp

--- 19-31.Mai/plot.py
+++ 19-31.Mai/plot.py
@@ -7,21 +7,72 @@ import scipy.constants as c
 import math
 from lmfit import minimize, Parameter, Model
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+#--------------------------------------------------------------------
+b = 0.00001666666666 #Umrechnungsfaktor von L/min in m^3/s
+cL = 1800 #m/s
+cP = 2700 #m/s
+c = 343.2 #m/s
+alpha1 = 1.40 #80.06° #Dopplerwinkel fü 15°
+alpha2 = 1.23 #70.53° #Dopplerwinkel fü 30°
+alpha3 = 0.96 #54.74° #Dopplerwinkel fü 60°
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+#--------------------------------------------------------------------
+def v(Q, d):
+    return Q / (math.pi * (d/2)**2)
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+def df(f, a):
+    return (f*c) / (4*10**6*math.cos(a))
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+#--------------------------------------------------------------------
+print('------------------------------------------------------------')
+Q, f1, f2, f3 = np.loadtxt('data1_1.txt', unpack=True)
+Q = Q / 10 * b
+d = 0.007 #Durchmesser in m
+
+print(f1/math.cos(alpha1))
+print(df(f1,alpha1))
+
+plt.plot(f1/math.cos(alpha1), df(f1,alpha1), 'rx')
+plt.xlabel(r'$\frac{\Delta\nu}{\cos(\alpha)}$ / Hz')
+plt.ylabel(r'v / $\frac{\text{m}}{\text{s}}')
+plt.tight_layout()
 plt.savefig('build/plot.pdf')
+plt.close()
+
+print('------------------------------------------------------------')
+Q, f1, f2, f3 = np.loadtxt('data1_2.txt', unpack=True)
+Q = Q / 10 * b
+d = 0.010 #Durchmesser in m
+print('v =', v, 'm/s')
+
+print('------------------------------------------------------------')
+Q, f1, f2, f3 = np.loadtxt('data1_3.txt', unpack=True)
+Q = Q / 10 * b
+d = 0.016 #Durchmesser in m
+print('v =', v, 'm/s')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print('------------------------------------------------------------')
