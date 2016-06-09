c7e9f0013900d197ffc491bca7b9df3b04ebbfa8
MaxSac <maximilian.sackel@tu-dortmund.de>
2016-06-09 12:56:54 +0200

    Rechnung

--- 20-07.Juni/plot.py
+++ 20-07.Juni/plot.py
@@ -1,27 +1,66 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
-from scipy.signal import find_peaks_cwt
 from uncertainties import ufloat
+from scipy import stats
 import scipy.constants as c
 import math
 from lmfit import minimize, Parameter, Model
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+print('Dicke des Vermewendeten Leiterplätchen berechnen')
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+#Spezifischer Wiederstand
+spRK = 0.018*10**(-6); spRZ = 0.06*10**(-6)
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+#liest die Spannung und Wiederstand ein zur Berechnung des Wiederstandes
+AK, VK = np.loadtxt('data/widerstand.txt', unpack=True)
+AZ, VZ = np.loadtxt('data/widerstandZink.txt', unpack=True)
+RK = [0]*10; RZ = [0]*16; VK *= 10**(-3); VZ *= 10**(-3)
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+#liest die Hallspannung ein
+AHK, VHK = np.loadtxt('data/hallK.txt', unpack=True)
+AHZ, VHZ = np.loadtxt('data/hallZ.txt', unpack=True)
+VHK *= 10**(-3); VHZ *= 10**(-3)
+
+#Berechnung des Wiederstandes
+def widerstand(R,V,A,a):
+    print('Einzelwiederstände:')
+    for x in range(a):
+        R[x] = V[x]/A[x]
+        print(x, ' = ', R[x])
+    print('Mittelwert = ', np.mean(R),' +- ', stats.sem(R))
+    return ufloat(np.mean(R), stats.sem(R))
+
+#Berechnung der Dicke des Plätchens
+def dicke(R,b,l,sp): 
+    k = (l*sp)/(R*b)
+    print('Dicke des Plättchens: ', k)
+    return k
+
+#Berechnet die Ladungsträgerdichte 
+def laddichte(I,U,d,a):
+    n = [0]*a
+    for x in range(a):
+        print(I[x], U[x],d,a, c.e)
+        n[x] = -5*I[x]/(U[x]*d*c.e)
+    d= ufloat(np.mean(n), stats.sem(n))
+    print('Die gemittelte Ladungsträgerdichte beträgt: ',d)
+    return d
+
+#Berechnet mittlere Driftgeschwindigkeit
+def mv(b,d,n):
+    print('Mittlere Driftgeschwindigkeit: ', -5/(b*d*n*c.e))
+    return
+
+print('Kupfer: ')
+rK = widerstand(RK,VK,AK,9)
+dK = dicke(rK, 0.02575, 0.0358, spRK)
+nK = laddichte(AHK, VHK, dK.n, 20)
+vK = mv(0.02575, dK, nK)
+
+print('Zink: ')
+rZ = widerstand(RZ,VZ,AZ,15)
+dZ = dicke(rZ, 0.02525, 0.0257, spRZ)
+nZ = laddichte(AHZ, VHZ, dZ.n, 16)
+vZ = mv(0.02525, dZ, nZ)
+
