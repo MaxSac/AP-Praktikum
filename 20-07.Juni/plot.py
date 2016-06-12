3251764ccc24a235fec532d8bffa636f4e2b0273
MaxSac <maximilian.sackel@tu-dortmund.de>
2016-06-12 17:39:54 +0200

    Auswertung

--- 20-07.Juni/plot.py
+++ 20-07.Juni/plot.py
@@ -8,7 +8,6 @@ import math
 from lmfit import minimize, Parameter, Model
 
 print('Dicke des Vermewendeten Leiterplätchen berechnen')
-'''
 #Spezifischer Wiederstand
 spRK = 0.018*10**(-6); spRZ = 0.06*10**(-6)
 
@@ -22,12 +21,15 @@ AHK, VHK = np.loadtxt('data/hallK.txt', unpack=True)
 AHZ, VHZ = np.loadtxt('data/hallZ.txt', unpack=True)
 VHK *= 10**(-3); VHZ *= 10**(-3)
 
+
+AB, HB, ZB = np.loadtxt('data/hysterese.txt', unpack=True)
+
 #Berechnung des Wiederstandes
 def widerstand(R,V,A,a):
     print('Einzelwiederstände:')
     for x in range(a):
         R[x] = V[x]/A[x]
-        print(x, ' = ', R[x])
+        #print(x, ' = ', R[x])
     print('Mittelwert = ', np.mean(R),' +- ', stats.sem(R))
     return ufloat(np.mean(R), stats.sem(R))
 
@@ -41,26 +43,73 @@ def dicke(R,b,l,sp):
 def laddichte(I,U,d,a):
     n = [0]*a
     for x in range(a):
-        print(I[x], U[x],d,a, c.e)
-        n[x] = -5*I[x]/(U[x]*d*c.e)
-    d= ufloat(np.mean(n), stats.sem(n))
-    print('Die gemittelte Ladungsträgerdichte beträgt: ',d)
-    return d
+        #print(I[x], U[x],d,a, c.e)
+        n[x] = -1.194*I[x]/(U[x]*d*c.e) 
+        print(x, 'Ladungsdichte', n[x])
+    print('Die gemittelte Ladungsträgerdichte beträgt: ', np.mean(n),' +-', stats.sem(n))
+    return np.mean(n) 
 
-#Berechnet mittlere Driftgeschwindigkeit
-def mv(b,d,n):
-    print('Mittlere Driftgeschwindigkeit: ', -5/(b*d*n*c.e))
-    return
+#Berechnet mittlere Flugzeit
+def tau(n, spR):
+    t = 2*c.m_e/((c.e**2)*n*spR)
+    print('Mittlere Flugzeit: ', t)
+    return t
+
+#mittlere Driftgeschwidigkeit
+def mv(n):
+    v = -1000*1000/(n*c.e)
+    print('Mittlere Driftgeschwindigkeit', v)
+    return v
+
+#Betrag der Geschwindigkeit der Elektronen im Festkörper
+def bv(n):
+    Ef = c.h**2/(2*c.m_e)*((3*n/(8*np.pi))**(2))**(1/3)
+    print('Ef', Ef)
+    v = np.sqrt(2*Ef/c.m_e)
+    print('Betrag der Geschwindigkeit', v)
+    return v
+
+# Mittlere freie Weglänge
+def l(tau, v):
+    l = tau*v
+    print('Mittlere Freie Weglänge', l)
+    return l
+
+#Berechnet beweglickeit 
+def my(tau):
+    print('Berechnet Beweglichkeit')
+    print(tau, c.e)
+    my = -c.e*tau/(2*c.m_e)
+    print(my)
 
 print('Kupfer: ')
-rK = widerstand(RK,VK,AK,9)
+rK = widerstand(RK,VK,AK,10)
 dK = dicke(rK, 0.02575, 0.0358, spRK)
-nK = laddichte(AHK, VHK, dK.n, 20)
-vK = mv(0.02575, dK, nK)
+nK = laddichte(AHK, VHK, dK.n, 19)
+tK = tau(nK, spRK)
+mvK = mv(nK)
+bvK = bv(nK)
+lK = l(tK, bvK)
+myK = my(tK)
+print('-------------------------------------------------------------')
 
 print('Zink: ')
-rZ = widerstand(RZ,VZ,AZ,15)
+rZ = widerstand(RZ,VZ,AZ,16)
 dZ = dicke(rZ, 0.02525, 0.0257, spRZ)
 nZ = laddichte(AHZ, VHZ, dZ.n, 16)
-vZ = mv(0.02525, dZ, nZ)
-'''
+tZ = tau(nZ, spRZ)
+mvZ = mv(nZ)
+bvZ = bv(nZ)
+lZ = l(tZ, bvZ)
+myZ = my(tZ)
+
+print('-------------------------------------------------------------')
+
+plt.plot(AB,HB, 'rx', label='Messwerte')
+plt.plot(AB,ZB, 'rx')
+plt.xlabel(r'Spulenstrom / A')
+plt.ylabel(r'Feldstärke / T')
+plt.legend(loc="best")
+plt.tight_layout()
+plt.savefig('build/Hysterese.pdf')
+plt.close()
