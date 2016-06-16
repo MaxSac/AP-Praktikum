ea78c50723f49d57811b6447c916e7c4a5ce497d
Philip Schäfers <phil.schaefers@gmail.com>
2016-06-16 18:21:07 +0200

    test

--- 21-14.Juni/plot.py
+++ 21-14.Juni/plot.py
@@ -12,6 +12,7 @@ U = np.array([350, 375, 400, 450, 500, 550, 600, 625, 650]) #Spannung in V
 A = np.array([5437, 5384, 5368, 5405, 5447, 5552, 5485, 5598, 5745]) #Impulsrate
 I = np.array([0.4, 0.6, 0.8, 1.2, 1.8, 2.4, 3.0, 3.6, 3.7, 4.0]) #Stromstärke in mikroA
 
+print('---------------------------------------------------------------------')
 i = 0
 Af = np.array([ufloat(0.0, 0.0), 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 while i < 9:
@@ -24,10 +25,14 @@ def Fit(x, m, b):
     return x * m + b
 model = Model(Fit, independent_vars=['x'])
 result = model.fit(A/10, x=U, b=Parameter(value=1), m=Parameter(value=0.1))
-print(result.params)
-
+print('Steigung =', 0.090864197530844279, '+-', 0.0222)
+print('y-Achse =', 503.69013096345441, '+-', 11.4)
+Stei = (ufloat(0.090864197530844279, 0.0222)*100) / 545
+print('Steigung in Prozent =', Stei*100)
+print('---------------------------------------------------------------------')
+'''
 xWert = np.linspace(300, 750, 1000)
-plt.errorbar(U, A/10, Af, fmt='o', ecolor='b', label='Messwerte')
+plt.errorbar(U, A/10, xerr=Af, fmt='o', ecolor='b', label='Messwerte')
 plt.plot(xWert, Fit(xWert, **result.params), 'r-', label='gefittete Plateaugerade')
 plt.xlabel('$U$ / V')
 plt.ylabel('Zählrate / 1/s')
@@ -36,6 +41,7 @@ plt.grid()
 plt.tight_layout()
 plt.savefig('build/Plateau.pdf')
 plt.close()
+'''
 
 
 print('---------------------------------------------------------------------')
